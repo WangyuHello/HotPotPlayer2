fee108f4a102d5f67980a73d4561d96a01a54efe
王渝 <wwangyuu@outlook.com>
2025-06-11 13:59:26 +0800

    添加项目文件。

---
+++ HotPotPlayer2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HotPotPlayer2.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ HotPotPlayer2/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace HotPotPlayer2.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
