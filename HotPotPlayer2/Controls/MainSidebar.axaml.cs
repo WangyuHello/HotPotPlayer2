0f851787d82b5965ed08932ce5aaa03ed5013e3d
王渝 <wwangyuu@outlook.com>
2025-06-12 17:12:42 +0800

    更新

--- HotPotPlayer2/Controls/MainSidebar.axaml.cs
+++ HotPotPlayer2/Controls/MainSidebar.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System;
 
 namespace HotPotPlayer2.Controls;
 
@@ -9,5 +11,57 @@ public partial class MainSidebar : UserControl
     public MainSidebar()
     {
         InitializeComponent();
+    }
+
+    public bool IsBackEnable
+    {
+        get { return (bool)GetValue(IsBackEnableProperty)!; }
+        set { SetValue(IsBackEnableProperty, value); }
+    }
+
+    public static readonly AvaloniaProperty<bool> IsBackEnableProperty =
+        AvaloniaProperty.Register<MainSidebar, bool>("IsBackEnable");
+
+
+    public string? SelectedPageName
+    {
+        get { return (string?)GetValue(SelectedPageNameProperty); }
+        set { SetValue(SelectedPageNameProperty, value); }
+    }
+
+    public static readonly AvaloniaProperty<string?> SelectedPageNameProperty =
+        AvaloniaProperty.Register<MainSidebar, string?>("SelectedPageName", coerce: OnSelectedPageNameChanged);
+
+    private static string? OnSelectedPageNameChanged(AvaloniaObject a, string? newValue)
+    {
+        var sidbar = (MainSidebar)a;
+        var newPageName = newValue;
+        sidbar._selectedButton = newPageName switch
+        {
+            string n when n.StartsWith("Music") => sidbar.Music,
+            string n when n.StartsWith("Video") => sidbar.Video,
+            string n when n.StartsWith("Bilibili") => sidbar.Bilibili,
+            string n when n.StartsWith("CloudMusic") => sidbar.CloudMusic,
+            string n when n.StartsWith("Setting") => sidbar.Setting,
+            _ => null,
+        };
+        return newValue;
+    }
+
+    Button? _selectedButton;
+    public event Action<string>? SelectedPageNameChanged;
+    public event Action? OnBackClick;
+
+    private void BackClick(object sender, RoutedEventArgs e)
+    {
+        OnBackClick?.Invoke();
+    }
+
+    private void NavigateClick(object sender, RoutedEventArgs e)
+    {
+        var b = (Button)sender;
+        var name = (string)b.Tag!;
+        SelectedPageNameChanged?.Invoke(name);
+        SelectedPageName = name;
     }
 }
---
+++ HotPotPlayer2/Converters/CommonConverters.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotPotPlayer2.Converters
+{
+    public static class CommonConverters
+    {
+        public static FuncMultiValueConverter<string?, bool> NavButtonEnable =>
+            new(names =>
+            {
+                var names2 = names.ToArray();
+                var name = names2[0];
+                var selectedName = names2[1];
+                if (name == null || selectedName == null) return true;
+                return !selectedName.StartsWith(name);
+            });
+    }
+}
